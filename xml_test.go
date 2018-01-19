fe5520bdabfbcfe55ed4e9554026f21cef4ece10
glen newton <glen.newton@gmail.com>
2018-01-19 04:09:30 -0500

    Refactored out test utilities from xml_test.go to tests_util.go

--- xml_test.go
+++ xml_test.go
@@ -1,15 +1,7 @@
 package main
 
 import (
-	//	"fmt"
-	"bytes"
-	"go/ast"
-	"go/importer"
-	"go/parser"
-	"go/token"
-	"go/types"
-	"log"
-	"strings"
+	//"log"
 	"testing"
 )
 
@@ -40,60 +32,4 @@ func TestGithubIssue14(t *testing.T) {
 	if err != nil {
 		t.Error(err)
 	}
-}
-
-/////////////////////////////////////////////////////////////////////////////
-func extractor(xmlStrings []string) error {
-	ex := Extractor{
-		namePrefix:              namePrefix,
-		nameSuffix:              nameSuffix,
-		useType:                 useType,
-		progress:                progress,
-		ignoreXmlDecodingErrors: ignoreXmlDecodingErrors,
-	}
-
-	ex.init()
-
-	for i, _ := range xmlStrings {
-		log.Println(xmlStrings[i])
-		err := ex.extract(strings.NewReader(xmlStrings[i]))
-
-		if err != nil {
-			return err
-		}
-	}
-
-	ex.done()
-
-	buf := bytes.NewBufferString("")
-	fps := make([]string, 1)
-	fps[0] = "foo"
-	generateGoCode(buf, fps, &ex)
-
-	log.Println(buf.String())
-
-	return parseAndType(buf.String())
-}
-
-// Derived from example at https://github.com/golang/example/tree/master/gotypes#an-example
-func parseAndType(s string) error {
-	fset := token.NewFileSet()
-	//f, err := parser.ParseFile(fset, "hello.go", hello, 0)
-	f, err := parser.ParseFile(fset, "hello.go", s, 0)
-	if err != nil {
-		//t.Error("Expected 1.5, got ", v)
-		//log.Fatal(err) // parse error
-		return err
-	}
-
-	conf := types.Config{Importer: importer.Default()}
-
-	// Type-check the package containing only file f.
-	// Check returns a *types.Package.
-	_, err = conf.Check("cmd/hello", fset, []*ast.File{f}, nil)
-	if err != nil {
-		//log.Fatal(err) // type error
-		return err
-	}
-	return nil
 }
